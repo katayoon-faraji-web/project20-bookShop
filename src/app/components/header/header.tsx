a25a69681327044bb11f2eb59aff6a7aaf2affa5
katayoon faraji <farajikatayoon75@gmail.com>
2024-02-28 14:15:46 +0330

    first commit

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header= ()=> {
+    return(
+        <header className='w-full h-[100px] font-extrabold flex justify-between items-center bg-red-400'>katy</header>
+    )
+}
+
+
+export default Header;
